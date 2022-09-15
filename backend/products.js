8469adc038d33f26d4cd8bf96c887cda0187fdae
yadhuvarshini <yadhuvarshini1@gmail.com>
2022-09-15 09:55:10 +0530

    first commit

---
+++ backend/products.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 14 Pro",
+    brand: "Apple",
+    desc: "6.7-inch display",
+    price: 190000,
+    image:
+      "https://images.hindustantimes.com/tech/img/2022/05/31/original/iPhone-14-Pro-Purple-Front-and-Back-MacRumors-Exclusive_1653967629359.jpeg",
+  },
+  {
+    id: 2,
+    name: "iPhone 14",
+    brand: "Apple",
+    desc: "6.5-inch mini display",
+    price: 150000,
+    image:
+      "https://img.etimg.com/thumb/width-1200,height-900,imgsize-19620,resizemode-1,msid-91187125/magazines/panache/iphone-14-pro-display-panel-to-feature-pill-and-hole-design-leaks-reveal-new-notch.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy Z flip 4",
+    brand: "Samsung",
+    desc: "7.6-inch display",
+    price: 150000,
+    image:"https://www.zdnet.com/a/img/resize/1c150a4468e2a55cbac1afd4138ac0389b800875/2022/08/12/01a97d89-3bfa-4f1c-a59a-d9d2e4d266d9/samsung-galaxy-z-fold-4-main-screen-2.jpg?auto=webp&width=768"
+  },
+];
+
+module.exports = products;
